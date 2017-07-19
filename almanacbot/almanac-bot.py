6007c5800ea1f59ac26417b5081323c0b40446ad
logoff <logoff@logoff.cat>
2017-07-19 20:33:26 +0200

    Add Twitter API client initialization.

--- almanacbot/almanac-bot.py
+++ almanacbot/almanac-bot.py
@@ -6,6 +6,10 @@ import sys
 
 import config
 import constants
+import twitter
+
+conf = None
+twitter_api = None
 
 
 def setup_logging(
@@ -24,13 +28,34 @@ def setup_logging(
         logging.basicConfig(level=log_level)
 
 
+def setup_twitter():
+    logging.info("Setting up Twitter API client...")
+
+    twitter_api = twitter.Api(
+        consumer_key=conf.config["twitter"]["consumer_key"],
+        consumer_secret=conf.config["twitter"]["consumer_secret"],
+        access_token_key=conf.config["twitter"]["access_token_key"],
+        access_token_secret=conf.config["twitter"]["access_token_secret"])
+
+    logging.info("Verifying Twitter API client credentials...")
+    twitter_api.VerifyCredentials()
+    logging.info("Twitter API client credentials verified.")
+
+    logging.info("Twitter API client set up.")
+
 if __name__ == '__main__':
     # configure logger
     setup_logging()
 
     # read configuration
     try:
-        configuration = config.Configuration(constants.CONFIG_FILE_NAME)
+        conf = config.Configuration(constants.CONFIG_FILE_NAME)
     except Exception as exc:
         logging.error("Error getting configuration.", exc)
+        sys.exit(1)
+
+    try:
+        setup_twitter()
+    except Exception as exc:
+        logging.error("Error setting up Twitter API client.", exc)
         sys.exit(1)
